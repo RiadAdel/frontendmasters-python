45ad99d05e7852b945baae1a480fdc73429bdf8a
Riad Adel Riad | Ejada Systems Ltd <radel@ejadacairo.local>
2020-12-10 15:29:20 +0200

    data types

---
+++ python fundamentals/data-types.py
@@ -0,0 +1,25 @@
+x = 10
+x = "string"
+
+# None type
+x = None
+
+# integers
+x = 10
+x = 1000_000  # 1000000 to be easy read
+
+# float
+x = 10.0
+
+# complex
+x = 3 + 10j
+
+# strings
+x = "Riad"
+x = "Riad"  # best practice
+x = """ Riad"""  # for keeping indentation and new lines
+# note: you can't concatenate strings with integers
+
+# f-strings
+x = 1  # you can concatenate integers here
+welcome = f"Welcome {x}"
