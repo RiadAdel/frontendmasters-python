8a4c2fa845bd901ec7953c2b952ccae0f173c8a9
Riad Adel Riad | Ejada Systems Ltd <radel@ejadacairo.local>
2020-12-10 14:02:09 +0200

    intro

---
+++ python fundamentals/intro.py
@@ -0,0 +1,11 @@
+name = "Riad"
+
+# show all methods
+dir(name)
+
+# help() with the type of var: explain functions
+help(str)
+
+# don't call your var name any reserved word
+list = 1
+# now you can't create list!
